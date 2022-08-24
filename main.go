8ed9786e07ac3711e5e7aa33cda11adc84863e8b
Reja <rezanurrochmat@gmail.com>
2022-08-24 18:31:51 +0700

    add handler fiber

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Hallo Golang. Long time no see :)")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Service is running!",
+		})
+	})
+
+	app.Listen(":8001")
 }

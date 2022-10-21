d876f35b1d97168ab831aed2e88b8a8aa8f15c94
Reja <rezanurrochmat@gmail.com>
2022-10-21 16:10:29 +0700

    add database cpnfig

--- config/database.go
+++ config/database.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDatabaseConnection(dataSourceName string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
+
+	if err != nil {
+		fmt.Errorf("Error when connect to database : %v", err.Error())
+		return nil, err
+	}
+
+	return db, nil
+}
